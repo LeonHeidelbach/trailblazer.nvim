b7a81688132a9a2a2ec05fde9f5c6c0fef8da0ce
Leon Heidelbach <leon.heidelbach@hhu.de>
2023-01-18 11:57:45 +0100

    chore: add new log message types

--- lua/trailblazer/log.lua
+++ lua/trailblazer/log.lua
@@ -18,20 +18,27 @@ Log.config = {
   },
   prefixes = {
     info = "TrailBlazer(INFO):",
+    warn = "TrailBlazer(WARN):",
     error = "TrailBlazer(ERROR):"
   }
 }
 
 Log.langs = {
   en = {
+    infos = {
+      current_trail_mark_select_mode = "Current trail mark select mode: ",
+    },
+    warnings = {
+      invalid_trail_mark_select_mode = "Invalid trail mark select mode. Please use one of the following: ",
+    },
     errors = {
       not_configured = "TrailBlazer is not configured. Please call `require('trailblazer').setup()` first.",
       unsupported_lang = "Unsupported language. Please use one of the following: " ..
           table.concat(Log.config.available_langs, ", "),
       invalid_buf_name = "Invalid buffer name. The current buffer has been used instead.",
-      invalid_pos_for_buf_lines = "Could not retrieve buffer lines for the current cursor position."
+      invalid_pos_for_buf_lines = "Could not retrieve buffer lines for the current cursor position.",
       duplicate_key_in_flatmap = "Duplicate key in flatmap.",
-    }
+    },
   }
 }
 
@@ -50,14 +57,26 @@ end
 
 --- Log an info message.
 ---@param name string
-function Log.info(name)
-  api.nvim_notify(Log.config.prefixes.info .. " " .. Log.current_lang[name], "info", {})
+---@param additional_info? string
+function Log.info(name, additional_info)
+  api.nvim_notify(Log.config.prefixes.info .. " " .. Log.current_lang.infos[name] ..
+    (tostring(additional_info) or ""), 0, {})
+end
+
+--- Log a warning message.
+---@param name string
+---@param additional_info? string
+function Log.warn(name, additional_info)
+  api.nvim_err_writeln(Log.config.prefixes.warn .. " " .. Log.current_lang.warnings[name] ..
+    (tostring(additional_info) or ""))
 end
 
 --- Log an error message.
 ---@param name string
-function Log.error(name)
-  error(Log.config.prefixes.error .. " " .. Log.current_lang.errors[name])
+---@param additional_info? string
+function Log.error(name, additional_info)
+  error(Log.config.prefixes.error .. " " .. Log.current_lang.errors[name] ..
+    (tostring(additional_info) or ""))
 end
 
 Log.setup(Log.config.default_lang)
