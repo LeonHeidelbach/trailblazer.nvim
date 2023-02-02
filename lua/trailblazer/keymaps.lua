59b3cc2ee1d1d11f9e1cbc18baa5da520ab53d08
Leon Heidelbach <leon.heidelbach@hhu.de>
2023-02-02 21:25:34 +0100

    feat: keymaps can now be properly disabled #15 #16
    Specific default keymaps as well as the entire mappings sub-table can
    now be disabled by setting their values to either an empty string or
    false.

--- lua/trailblazer/keymaps.lua
+++ lua/trailblazer/keymaps.lua
@@ -7,6 +7,8 @@
 --- This module is responsible for setting up keymaps.
 ---@brief ]]
 
+local fn = vim.fn
+
 local log = require("trailblazer.log")
 local Keymaps = {}
 
@@ -19,6 +21,7 @@ Keymaps.config = {
 ---@param key_maps table
 ---@param tb_api? table
 function Keymaps.register_api_maps(key_maps, tb_api)
+  if not key_maps then return end
   Keymaps.register_for_buf(key_maps, "trailblazer", tb_api, nil,
     function(_, callback)
       log.warn("invalid_trailblazer_api_callback", callback)
@@ -33,26 +36,29 @@ end
 ---@param buf? number
 ---@param warn_callback? function
 function Keymaps.register_for_buf(key_maps, mod_name, mod, buf, warn_callback)
+  if not key_maps then return end
   for mode_label, mode_maps in pairs(key_maps) do
     for mode in mode_label:gmatch(".") do
       for _, map_type in pairs(mode_maps) do
         for callback, map in pairs(map_type) do
-          if not mod or mod and mod[callback] ~= nil then
-            local cmd = string.format("<cmd>lua require('" .. mod_name .. "').%s()<CR>", callback)
-            local opts = Keymaps.config.default_map_opts
+          if map and fn.empty(map) == 0 then
+            if not mod or mod and mod[callback] ~= nil then
+              local cmd = string.format("<cmd>lua require('" .. mod_name .. "').%s()<CR>", callback)
+              local opts = Keymaps.config.default_map_opts
 
-            if buf then
-              opts = vim.deepcopy(Keymaps.config.default_map_opts)
-              opts.buffer = buf
-            end
+              if buf then
+                opts = vim.deepcopy(Keymaps.config.default_map_opts)
+                opts.buffer = buf
+              end
 
-            vim.keymap.set(mode, map, cmd, opts)
-          else
-            if warn_callback then
-              warn_callback(mod_name, callback)
+              vim.keymap.set(mode, map, cmd, opts)
             else
-              log.warn("invalid_trailblazer_mod_callback_register", "[ " .. callback .. " | " ..
-                mod_name .. " ]")
+              if warn_callback then
+                warn_callback(mod_name, callback)
+              else
+                log.warn("invalid_trailblazer_mod_callback_register", "[ " .. callback .. " | " ..
+                  mod_name .. " ]")
+              end
             end
           end
         end
@@ -69,25 +75,28 @@ end
 ---@param buf? number
 ---@param warn_callback? function
 function Keymaps.unregister_for_buf(key_maps, mod_name, mod, buf, warn_callback)
+  if not key_maps then return end
   for mode_label, mode_maps in pairs(key_maps) do
     for mode in mode_label:gmatch(".") do
       for _, map_type in pairs(mode_maps) do
         for callback, map in pairs(map_type) do
-          if not mod or mod and mod[callback] ~= nil then
-            local opts = Keymaps.config.default_map_opts
+          if map and fn.empty(map) == 0 then
+            if not mod or mod and mod[callback] ~= nil then
+              local opts = Keymaps.config.default_map_opts
 
-            if buf then
-              opts = vim.deepcopy(Keymaps.config.default_map_opts)
-              opts.buffer = buf
-            end
+              if buf then
+                opts = vim.deepcopy(Keymaps.config.default_map_opts)
+                opts.buffer = buf
+              end
 
-            pcall(vim.keymap.del, mode, map, opts)
-          else
-            if warn_callback then
-              warn_callback(mod_name, callback)
+              pcall(vim.keymap.del, mode, map, opts)
             else
-              log.warn("invalid_trailblazer_mod_callback_unregister", "[ " .. callback .. " | " ..
-                mod_name .. " ]")
+              if warn_callback then
+                warn_callback(mod_name, callback)
+              else
+                log.warn("invalid_trailblazer_mod_callback_unregister", "[ " .. callback .. " | " ..
+                  mod_name .. " ]")
+              end
             end
           end
         end
