5b4feb002358d509fe753f91f6e588a2161fafe4
Leon Heidelbach <leon.heidelbach@hhu.de>
2023-02-13 14:08:08 +0100

    fix: `:TrailBlazerNewTrailMark` now properly toggles trail marks

--- plugin/trailblazer.lua
+++ plugin/trailblazer.lua
@@ -38,8 +38,10 @@ end
 
 -- User commands
 api.nvim_create_user_command("TrailBlazerNewTrailMark",
-  function(args) tb.new_trail_mark(tonumber(args.fargs[1]), tonumber(args.fargs[2]),
-      { tonumber(args.fargs[3]), tonumber(args.fargs[4]) })
+  function(args)
+    local pos = args.fargs[3] and args.fargs[4] and
+        { tonumber(args.fargs[3]), tonumber(args.fargs[4]) } or nil
+    tb.new_trail_mark(tonumber(args.fargs[1]), tonumber(args.fargs[2]), pos)
   end, { nargs = "*" })
 
 api.nvim_create_user_command("TrailBlazerTrackBack", function(args) tb.track_back(args.args) end,
